7949bf2c000d219881df1ed270360aef4eab2d47
Brian Stroh <40881137+brianjstroh@users.noreply.github.com>
2019-02-20 19:09:08 -0800

    Initial Commit
    Initial Commit

---
+++ Coursera_DataMiningSpecialization/Course1_DataVisualization/Week2/ProgrammingAssignment1/Brian_Script.R
@@ -0,0 +1,188 @@
+library(leaflet)
+library(reshape2)
+library(colorRamps)
+library(dplyr)
+library(shiny)
+
+setwd("C:\\Users\\brian.stroh\\Desktop\\temp_wd")
+unzip("rawdata.zip")
+GISTEMP.data <-read.csv(file = unzip("rawdata.zip",list = TRUE)$Name[2])
+flat.temps<-melt(GISTEMP.data, id.vars = names(GISTEMP.data)[1], variable.name = "Splits", value.name = "Temp.Diffs")
+flat.temps$Temp.Diffs<-pmax(rep(-max(flat.temps$Temp.Diffs),nrow(flat.temps)),flat.temps$Temp.Diffs)+max(flat.temps$Temp.Diffs)
+
+ui<-fluidPage(
+      pageWithSidebar(
+            headerPanel(title = "Global Temperatures from 1980-2014"),
+            sidebarPanel(
+                  sliderInput("curr.year", "Year of Study:", value = 1950, min = 1880, max = 2014, step = 1),
+                  radioButtons(inputId = "temp.layers", "Temperature Layer:", choices = c("Global", "Hemispheric", "3-way Split", "8-way Split"), selected = "8-way Split")
+            ),
+            mainPanel(
+                  leafletOutput("my.map")
+            )
+      )
+)
+
+
+server <-function(input, output, session) {
+      
+      my.colors <- blue2red(422)
+      # my.layer <- renderText(input$temp.layers) 
+      
+      map.init <- reactive({
+            leaflet() %>% 
+                  addTiles() %>%
+                  fitBounds(-180, -90, 180, 90) 
+      })
+      
+      output$my.map <- renderLeaflet({
+            user.map()
+      })
+      
+      user.map <- reactive({
+            if(input$temp.layers == "Global"){
+                  user.map <- map.init() %>%
+                        addRectangles(
+                              lng1=-1500, lat1=-90,
+                              lng2=1500, lat2=90,
+                              group = "Global",
+                              layerId = "Globe",
+                              color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "Glob")$Temp.Diffs],
+                              opacity = 0,
+                              fillOpacity = .35
+                        )
+                  return (user.map)
+            } else if(input$temp.layers == "Hemispheric"){
+                  user.map <- map.init() %>%
+                  addRectangles(
+                        lng1=-1500, lat1=0,
+                        lng2=1500, lat2=90,
+                        group = "Hemisphere.Splits",
+                        layerId = "NHemi",
+                        color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "NHem")$Temp.Diffs],
+                        opacity = 0,
+                        fillOpacity = .35
+                  ) %>%
+                  addRectangles(
+                        lng1=-1500, lat1=-90,
+                        lng2=1500, lat2=0,
+                        group = "Hemisphere.Splits",
+                        layerId = "SHemi",
+                        color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "SHem")$Temp.Diffs],
+                        opacity = 0,
+                        fillOpacity = .35
+                  )
+                  return (user.map)
+            } else if(input$temp.layers == "3-way Split"){
+                  user.map <- map.init() %>%
+                        addRectangles(
+                        lng1=-1500, lat1=-90,
+                        lng2=1500, lat2=-24,
+                        group = "3.Splits",
+                        layerId = "3.1",
+                        color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "X90S.24S")$Temp.Diffs],
+                        opacity = 0,
+                        fillOpacity = .35
+                  ) %>%
+                        addRectangles(
+                              lng1=-1500, lat1=-24,
+                              lng2=1500, lat2=24,
+                              group = "3.Splits",
+                              layerId = "3.2",
+                              color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "X24S.24N")$Temp.Diffs],
+                              opacity = 0,
+                              fillOpacity = .35
+                        ) %>%
+                        addRectangles(
+                              lng1=-1500, lat1=24,
+                              lng2=1500, lat2=90,
+                              group = "3.Splits",
+                              layerId = "3.3",
+                              color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "X24N.90N")$Temp.Diffs],
+                              opacity = 0,
+                              fillOpacity = .35
+                        )
+                  return (user.map)
+            }  else if(input$temp.layers == "8-way Split"){
+                  user.map <- map.init() %>%
+                  addRectangles(
+                        lng1=-1500, lat1=-90,
+                        lng2=1500, lat2=-64,
+                        group = "8.Splits",
+                        layerId = "8.1",
+                        color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "X90S.64S")$Temp.Diffs],
+                        opacity = 0,
+                        fillOpacity = .35
+                  ) %>%
+                        addRectangles(
+                              lng1=-1500, lat1=-64,
+                              lng2=1500, lat2=-44,
+                              group = "8.Splits",
+                              layerId = "8.2",
+                              color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "X64S.44S")$Temp.Diffs],
+                              opacity = 0,
+                              fillOpacity = .35
+                        ) %>%
+                        addRectangles(
+                              lng1=-1500, lat1=-44,
+                              lng2=1500, lat2=-24,
+                              group = "8.Splits",
+                              layerId = "8.3",
+                              color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "X44S.24S")$Temp.Diffs],
+                              opacity = 0,
+                              fillOpacity = .35
+                        ) %>%
+                        addRectangles(
+                              lng1=-1500, lat1=-24,
+                              lng2=1500, lat2=0,
+                              group = "8.Splits",
+                              layerId = "8.4",
+                              color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "X24S.EQU")$Temp.Diffs],
+                              opacity = 0,
+                              fillOpacity = .35
+                        ) %>%
+                        addRectangles(
+                              lng1=-1500, lat1=0,
+                              lng2=1500, lat2=24,
+                              group = "8.Splits",
+                              layerId = "8.5",
+                              color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "EQU.24N")$Temp.Diffs],
+                              opacity = 0,
+                              fillOpacity = .35
+                        ) %>%
+                        addRectangles(
+                              lng1=-1500, lat1=24,
+                              lng2=1500, lat2=44,
+                              group = "8.Splits",
+                              layerId = "8.6",
+                              color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "X24N.44N")$Temp.Diffs],
+                              opacity = 0,
+                              fillOpacity = .35
+                        ) %>%
+                        addRectangles(
+                              lng1=-1500, lat1=44,
+                              lng2=1500, lat2=64,
+                              group = "8.Splits",
+                              layerId = "8.7",
+                              color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "X44N.64N")$Temp.Diffs],
+                              opacity = 0,
+                              fillOpacity = .35
+                        ) %>%
+                        addRectangles(
+                              lng1=-1500, lat1=64,
+                              lng2=1500, lat2=90,
+                              group = "8.Splits",
+                              layerId = "8.8",
+                              color = my.colors[filter(flat.temps, Year == input$curr.year, Splits == "X64N.90N")$Temp.Diffs],
+                              opacity = 0,
+                              fillOpacity = .35
+                        ) 
+                  return (user.map)
+            }
+      })
+      
+      
+}
+
+
+shinyApp(ui, server)
